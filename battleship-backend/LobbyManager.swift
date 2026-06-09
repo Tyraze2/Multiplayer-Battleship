d755436d5bcd7f0d71fe919703bd76858b8d953d
Tyraze2 <valentind@lyceechurchill.london>
2026-06-09 16:31:50 +0100

    id room made

--- battleship-backend/LobbyManager.swift
+++ battleship-backend/LobbyManager.swift
@@ -1,1 +1,11 @@
+import Foundation
+import Vapor
 
+class Lobby {
+    var id: String
+    var player1: WebSocket? = nil
+    var player2: WebSocket? = nil
+    init(id: UUID){
+        self.id = id
+    }
+}
