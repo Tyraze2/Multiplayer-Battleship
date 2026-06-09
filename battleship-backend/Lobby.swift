f52462de9a43b0d7a8586204068d76bb4c7c9334
Tyraze2 <valentind@lyceechurchill.london>
2026-06-09 07:40:38 +0100

    Added player1 class for game lobby
    Introduced player1 class with status management and properties.

---
+++ battleship-backend/Lobby.swift
@@ -0,0 +1,13 @@
+import Foundation
+
+class player1 {
+    enum p1Status {
+        case matchmaking
+        case idle
+        case inMatch
+    }
+    let p1Username = ""
+    var isInGame = false
+    let p1ID = UUID()
+}
+    
