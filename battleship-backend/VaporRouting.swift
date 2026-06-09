d5991f07ef9b00d24ceabe86b8ed6f97717e7c98
Tyraze2 <valentind@lyceechurchill.london>
2026-06-09 15:27:40 +0100

    Update VaporRouting.swift

--- battleship-backend/VaporRouting.swift
+++ battleship-backend/VaporRouting.swift
@@ -5,10 +5,29 @@ var playersOnline: [WebSocket] = []
 func APIConnection(_ app: Application) throws {
     app.webSocket("ws"){
         req, ws in
+
+        playersOnline.append(ws)
         ws.send("Connected")
+
         ws.onText {
             ws, text in
-            print("Recived \(text)")
+            guard let data = text.data(using: .utf8) else {return}
+do {
+                let packet = try JSONDecoder().decode(GamePacket.self, from: data)
+                
+                if packet.action == "join" {
+                    let lobbyUpdate = ["status": "lobby_updated"]
+                    let encodedData = try JSONEncoder().encode(lobbyUpdate)
+                    
+                    if let jsonString = String(data: encodedData, encoding: .utf8) {
+                        for connection in activeConnections {
+                            connection.send(jsonString)
+                        }
+                    }
+                }
+            } catch {
+                print("Error")
+            }
         }
     }
 }
