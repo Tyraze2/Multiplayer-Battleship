c3d02a2e9a1e8f631ce3e44561f03016e4fda390
skysplatwoomy <portheaultp@gmail.com>
2026-06-10 19:52:52 +0100

    list wow

--- battleship-frontend/lobby/lobby.js
+++ battleship-frontend/lobby/lobby.js
@@ -1,20 +1,28 @@
 const socket = new WebSocket("ws://localhost:8080/ws");
 
-// 1. Fixed array syntax
+socket.addEventListener('message', event => {
+    const packet = JSON.parse(event.packet);
+    console.log(packet);
+
+    if (packet == "active_lobbies") {
+        lobbyUI(packet);
+    }
+});
+
 const games = ["test", "test2"]; 
 
 socket.addEventListener('open', (event) => {
     console.log("by pure hopes and dreams we somehow connected mashallah");
 });
 
-// 2. Fixed loop syntax (.length instead of len())
-for (let i = 0; i < games.length; i++) {
-    const gameSelect = document.createElement('div');
-    gameSelect.textContent = games[i];
+function lobbyUI(games) {
+    const gamesContainer = document.querySelector('#lobby-list')
     
-    gameSelect.addEventListener('click', () => {
-        console.log(`joined game ${games[i]}`);
-    });
-    
-    document.querySelector('#games').appendChild(gameSelect);
+    for (let i = 0; i < games.length; i++) {
+        const game = document.createElement('li');
+        game.textContent = games[i];
+        gamesContainer.appendChild(game);
+    }
 }
+
+lobbyUI(games);
