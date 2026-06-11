f6e39980e35eb24717f272bb76d9e70f0d891c4d
Tyraze2 <valentind@lyceechurchill.london>
2026-06-11 14:03:43 +0100

    Started lobby manager

--- battleship-backend/LobbyManager.swift
+++ battleship-backend/LobbyManager.swift
@@ -5,7 +5,13 @@ class Lobby {
     var id: String
     var player1: WebSocket? = nil
     var player2: WebSocket? = nil
+    var isFull = true 
     init(id: String){
         self.id = id
     }
+}
+
+class LobbyManager {
+    static let shared = LobbyManager()
+    private init(){}
 }
