0e9df34b95489902f7a94c326ea76a39598c3bc7
Tyraze2 <valentind@lyceechurchill.london>
2026-06-12 11:13:39 +0100

    Update player properties to use Player type

--- battleship-backend/LobbyManager.swift
+++ battleship-backend/LobbyManager.swift
@@ -3,8 +3,8 @@ import Vapor
 
 class Lobby {
     var id: String
-    var player1?
-    var player2?
+    var player1: Player?
+    var player2: Player?
     var isFull: Bool {
         if player1 != nil && player2 != nil {
             return true
