992eb5002553e15c020308995e3545f0a6237c67
skysplatwoomy <portheaultp@gmail.com>
2026-06-12 15:29:59 +0100

    join button actually joins now

--- battleship-frontend/lobby/lobby.js
+++ battleship-frontend/lobby/lobby.js
@@ -1,10 +1,10 @@
 const socket = new WebSocket("ws://localhost:8080/ws");
 
 socket.addEventListener('message', event => {
-    const packet = JSON.parse(event.packet);
+    const packet = JSON.parse(event.data);
     console.log(packet);
 
-    if (packet == "active_lobbies") {
+    if (packet.status == "active_lobbies") {
         lobbyUI(packet); // every time recieving new lobbies, UI should be updated (unless it hates me)
     }
 });
@@ -15,7 +15,6 @@ const games = [
     {name: "test3", players: 0 },
     {name: "test4", players: 3 }
 ]; // barebones and not the way packets are gonna be recieved but good enough to test for now
-
 
 socket.addEventListener('open', (event) => {
     console.log("by pure hopes and dreams we somehow connected mashallah");
@@ -31,22 +30,27 @@ function lobbyUI(games) {
         const game = document.createElement('li');
         game.classList.add('game');
         if ( games[i].players == 2 ) {
-            game.innerHTML = `<span>${sortedGames[i].name}</span><span class="full-status">Full</span>`;
+            game.innerHTML = `<span>${sortedGames[i].name}'s room</span><span class="full-status">Full</span>`;
         } // full game nono!
         else if (games[i].players > 2) {
-            game.innerHTML = `<span>${sortedGames[i].name}</span><span class="ivalid">Invalid Game</span>`;
+            game.innerHTML = `<span>${sortedGames[i].name}'s room</span><span class="ivalid">Invalid Game</span>`;
         } // no handling for sorting these to be at the btotom but lowk this will never happen i think so it's fine
         else if (games[i].players < 1) {
-            game.innerHTML = `<span>${sortedGames[i].name}</span><span class="ivalid">Invalid Game</span>`;
+            game.innerHTML = `<span>${sortedGames[i].name}'s room</span><span class="ivalid">Invalid Game</span>`;
         }
         else {
-            game.innerHTML = `<span>${sortedGames[i].name}</span><button class="join-btn">Join</button>`
+            game.innerHTML = `<span>${sortedGames[i].name}'s room</span><button class="join-btn">Join</button>`
         } // not full game yesyes!
         
         gamesContainer.appendChild(game); // they had baby child
+        
+        const joinBtn = game.querySelector('.join-btn');
+        if (joinBtn) {
+            joinBtn.addEventListener('click', (event) => {
+                window.location.href = `../game/game.html?game=${encodeURIComponent(sortedGames[i].name)}`;
+            });
+        }
     }
 }
-
-// ok im gonna leave for school now remind to do this
 
 lobbyUI(games);
