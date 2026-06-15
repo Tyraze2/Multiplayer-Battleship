9983cbb98bb324a0be03711604a098028c23ec27
Tyraze2 <valentind@lyceechurchill.london>
2026-06-15 16:15:45 +0100

    Lobby parameters

--- battleship-backend/LobbyManager.swift
+++ battleship-backend/LobbyManager.swift
@@ -26,7 +26,7 @@ class LobbyManager {
         let newLobby = Lobby(id: id)
         lobbyList[id] = newLobby
     }
-    func AddPlayerToLobby() {
+    func AddPlayerToLobby(lobbyID: String, player: String) {
         guard Lobby.player1 == nil || Lobby.player2 == nil 
         else {
             return
